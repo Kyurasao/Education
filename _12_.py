85aa7b46701ba17b482831c76379284c2cc5e874
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-02-13 14:24:05 +0400

    Done

--- _12_.py
+++ _12_.py
@@ -9,7 +9,7 @@ NAME = [
 CHAR = 'qwertyuiopasdfghjklzxcvbnm'
 
 
-def foo(s):
+def check_char(s):
     for el in s:
         if el not in CHAR:
             return False
@@ -18,7 +18,7 @@ def foo(s):
 
 def main():
     for el in NAME:
-        if '.' in el and len(el) != el.rfind('.') + 1 and foo(el[el.rfind('.') + 1:].lower()):
+        if '.' in el and len(el) != el.rfind('.') + 1 and check_char(el[el.rfind('.') + 1:].lower()):
             print(f'имя файла - "{el}"; расширение файла - {el[el.rfind(".") + 1:]}')
         else:
             print(f'"{el}" - некорректное имя файла')
