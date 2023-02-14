066abc8326b85a288a6a016f1d5be1bfd6309282
Nikita Serditov <34740100+mashida@users.noreply.github.com>
2023-02-14 13:04:10 +0300

    принято

--- _13_.py
+++ _13_.py
@@ -2,8 +2,9 @@ N = 6
 
 
 def main(n: int) -> int:
-    print(f'сумма для N = {n}: {n + n * n + n * n * n}')
+    print(f'сумма для N = {N}: {n + n * n + n * n * n}') 
 
 
-if __name__ == '__main__':
+
+if __name__ == '__main__': # принято!
     main(N)
