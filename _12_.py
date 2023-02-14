3b334555d30ba00def1c63c4b2a19e4cc5e93266
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-02-14 05:00:20 +0400

    Done

--- _12_.py
+++ _12_.py
@@ -6,19 +6,11 @@ NAME = [
     'aSdFghjkLqwE',
     ''
 ]
-CHAR = 'qwertyuiopasdfghjklzxcvbnm'
 
 
-def check_char(s):
-    for el in s:
-        if el not in CHAR:
-            return False
-    return True
-
-
-def main():
+def main():     # нужно ли тут указывать тип данных результата??????????
     for el in NAME:
-        if '.' in el and len(el) != el.rfind('.') + 1 and check_char(el[el.rfind('.') + 1:].lower()):
+        if '.' in el and len(el) != el.rfind('.') + 1 and el[el.rfind('.') + 1:].isalpha():
             print(f'имя файла - "{el}"; расширение файла - {el[el.rfind(".") + 1:]}')
         else:
             print(f'"{el}" - некорректное имя файла')
--- _14_.py
+++ _14_.py
@@ -1,11 +1,14 @@
-N = [1, 2, 3, 4, 230, 5, 6, 237, 238, 7, 8, 9, 10]
+N = [1, 2, 3, 4, 230, 5, 6, 238, 237, 7, 8, 9, 10]
 
 
-def main():
+def main():    # нужно ли тут указывать тип данных результата??????????
     res = []
-    for el in N[:N.index(237)]:
-        if el % 2 == 0:
-            res.append(el)
+    if 237 in N:
+        for el in N[:N.index(237)]:
+            if el % 2 == 0:
+                res.append(el)
+    else:
+        return print(f'число 237 не встречается в списке {N}')
     print(f'результат: {res}')
 
 
