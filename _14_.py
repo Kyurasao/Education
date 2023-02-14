d2500c6214f8d9fa5f254a4eb9f051b648906200
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-02-15 02:54:16 +0400

    Done

--- _14_.py
+++ _14_.py
@@ -1,14 +1,15 @@
-N = [[1, 2, 3, 4, 230, 5, 6, 238, 23, 7, 8, 9, 10], [1, 2, 3, 4, 230, 5, 6, 238, 237, 7, 8, 9, 10], [0, -2, 4, 237]]
+N = [[3, 4, 230, 5, 6, 237, 23, 7, 8], [1, 230, 5, 6, 238, 23, 7, 8, 9], [0, -22, -7, 4, 237, 10]]
 
 
 def main() -> None:
-    for elem in N:
+    for list_el in N:
         res = []
-        if 237 in elem: # << что будет если в списке нет элемента 237?
-            for el in elem[:elem.index(237)]:
-                if el % 2 == 0:
-                    res.append(el)
-        print(f'результат - {res if res else "число 237 не встречается в списке"}')
+        for el in list_el:
+            if el == 237:
+                break
+            if el % 2 == 0:
+                res.append(el)
+        print(f'результат - {res}')
 
 
 if __name__ == '__main__':
