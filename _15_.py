39f324c1c3718f4909c8c69bbb8d8f90a8a74711
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-02-14 13:48:57 +0400

    Done

--- _15_.py
+++ _15_.py
@@ -4,12 +4,9 @@ SECOND = [0, 2, 3, 15, 8, 9, 10, 1, 1]
 
 def main():
     for el_2 in SECOND:
-        print(f'2й список - текущий элемент - {el_2}')
         if el_2 in FIRST:
-            print(f'список FIRST до удаления - {FIRST}')
             FIRST.remove(el_2)
-            print(f'список FIRST после удаления - {FIRST}')
-    print(f'список FIRST - результат - {FIRST}')
+    print(f'результат - {FIRST}')
 
 
 if __name__ == '__main__':
