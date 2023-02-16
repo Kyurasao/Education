fd33a9eb12c2c00d0586f5c9a3219d777dbf1a10
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-02-16 13:56:47 +0400

    Done

---
+++ _21_.py
@@ -0,0 +1,18 @@
+LIST_OF_SEQUENCE = [
+    [1, 10, 30, 5, 5],
+    [1, 10, 30, 5, 45],
+    (0, 4, 6, 7, 9),
+    (5, 6, 5, 7)
+]     # строки ведь не нужно добавлять в список последовательностей??
+
+
+def main(list_1: list) -> None:
+    for seq in list_1:
+        count = 0
+        for el in seq:
+            count += seq.count(el)
+        print(f'числа последовательности "{seq}" {"уникальны" if count == len(seq) else "не уникальны"}')
+
+
+if __name__ == '__main__':
+    main(LIST_OF_SEQUENCE)
