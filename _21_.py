53b0189526d05c61d184f5de4677f2df3ef751c0
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-02-16 23:02:11 +0400

    Done

--- _21_.py
+++ _21_.py
@@ -3,16 +3,13 @@ LIST_OF_SEQUENCE = [
     [1, 10, 30, 5, 45],
     (0, 4, 6, 7, 9),
     (5, 6, 5, 7)
-]     # строки ведь не нужно добавлять в список последовательностей??
+]
 
 
 def main(list_1: list) -> None:
     for seq in list_1:
-        count = 0
-        for el in seq:
-            count += seq.count(el)
-        print(f'числа последовательности "{seq}" {"уникальны" if count == len(seq) else "не уникальны"}')
-    # а какая структура данных хранит только уникальные символы?
+        print(f'числа последовательности "{seq}" {"уникальны" if len(set(seq)) == len(seq) else "не уникальны"}')
+
 
 if __name__ == '__main__':
     main(LIST_OF_SEQUENCE)
