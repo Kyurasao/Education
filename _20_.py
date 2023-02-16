fb3e63f6f608b46c77a18d5789c27bd8e9027441
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-02-16 13:50:16 +0400

    Done

--- _20_.py
+++ _20_.py
@@ -1,8 +1,8 @@
 LIST_OF_NUMBERS = [1, 10, 30, 5, 45]
 
 
-def main(l_1: list) -> None:
-    print(f'элементы кратные 15: {list(filter(lambda x: (x % 15 == 0), l_1))}')
+def main(list_1: list) -> None:
+    print(f'элементы кратные 15: {list(filter(lambda x: (x % 15 == 0), list_1))}')
 
 
 if __name__ == '__main__':
