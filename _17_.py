60f50c61cd76e3be77bafd3a5d4f172045d7b3fc
Nikita Serditov <34740100+mashida@users.noreply.github.com>
2023-02-16 15:09:06 +0300

    Update _17_.py
    Вопрос

--- _17_.py
+++ _17_.py
@@ -5,7 +5,8 @@ def main(num: int) -> None:
     res = 0
     for c in str(num):
         res += int(c)
-    print(f'сумма цифр целого числа {num} = {res}')
+    print(f'сумма цифр целого числа {num} = {res}') 
+    # а как можно сделать это же, но без цикла?
 
 
 if __name__ == '__main__':
