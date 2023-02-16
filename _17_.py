868612835ec99aa3d806ad650dc857bd44e549c5
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-02-16 22:41:16 +0400

    Done

--- _17_.py
+++ _17_.py
@@ -2,11 +2,7 @@ NUM = 123456
 
 
 def main(num: int) -> None:
-    res = 0
-    for c in str(num):
-        res += int(c)
-    print(f'сумма цифр целого числа {num} = {res}') 
-    # а как можно сделать это же, но без цикла?
+    print(f'сумма цифр целого числа {num} = {sum(int(c) for c in str(num))}')
 
 
 if __name__ == '__main__':
