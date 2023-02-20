f6bc6036d2e2cb6742134029af14734ae15ed730
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-02-20 14:37:11 +0400

    На проверку

--- _5_.py
+++ _5_.py
@@ -9,6 +9,7 @@ MY_DICT = {'a': 500, 'b': 5874, 'c': 560, 'd': 400, 'e': 5874, 'f': 20}
 
 def main(d: dict) -> None:
     sort_list = sorted(d.values())
+    print(sort_list)
     print(f'В словаре {MY_DICT}\nтри ключа с максимальными значениями: {[key for key in d.keys() if d[key] >= sort_list[-3]]}')
 
 
